4f4702c9c5c61c84c6a2fe078dbf9dd6efc5b0b8
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-12 20:54:20 +0530

    express complete

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
+const path = require('path')
 const express = require('express')
+
+const rootdir = require('../util/path')
 
 const router = express.Router()
 
 // admin/add-product   GET method
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">ADD PRODUCT</button></form>')
+    res.sendFile(path.join(rootdir, 'views', 'add-product.html'))
 })
 
 // admin/add-product   POST method
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
+const path = require('path')
 const express = require('express')
+
+const rootdir = require('../util/path')
+
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>')
+    res.sendFile(path.join(rootdir, 'views', 'shop.html'))
 })
 
 module.exports = router
