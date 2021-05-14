ad067f9a8e5c1cd0ee641953ec3d1e51809274c1
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-14 19:48:42 +0530

    ejs complete

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// for handlebars
-app.engine('hbs', expressHbs({layoutsDir:'./views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}))
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+// for ejs
+app.set('view engine', 'ejs')
+app.set('views','views')
+
+// for express-handlebars
+// app.engine('hbs', expressHbs({layoutsDir:'./views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}))
+// app.set('view engine', 'hbs')
+// app.set('views', 'views')
 
 // for pug
 // app.set('view engine', 'pug');
@@ -25,7 +29,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle : "Page not found"})
+    res.status(404).render('404', {pageTitle : "Page not found", path:''})
 });
 
 app.listen(3000);
