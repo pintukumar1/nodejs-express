7045d6acc5cb38b9ba2fd59d21f7963cccbb3007
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-14 17:15:28 +0530

    hbs complete

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const path = require('path');
-
 const express = require('express');
+const expressHbs = require('express-handlebars')
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+// for handlebars
+app.engine('hbs', expressHbs({layoutsDir:'./views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+// for pug
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,12 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop Page', path: '/'});
+  res.render('shop', {
+    prods: products, 
+    pageTitle: 'Shop Page', 
+    path: '/', hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true});
 });
 
 module.exports = router;
