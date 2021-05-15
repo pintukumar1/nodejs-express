5ed1186726b51d54e7bf8b1b359514cd8cb158d0
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-15 10:29:38 +0530

    mvc complete

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
 const path = require('path');
 const express = require('express');
-const expressHbs = require('express-handlebars')
 const bodyParser = require('body-parser');
 
 const app = express();
+const errorController = require('./controllers/error')
 
-// for ejs
 app.set('view engine', 'ejs')
 app.set('views','views')
 
-// for express-handlebars
-// app.engine('hbs', expressHbs({layoutsDir:'./views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}))
-// app.set('view engine', 'hbs')
-// app.set('views', 'views')
-
-// for pug
-// app.set('view engine', 'pug');
-// app.set('views', 'views');
-
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle : "Page not found", path:''})
-});
+app.use(errorController.get404);
 
 app.listen(3000);
