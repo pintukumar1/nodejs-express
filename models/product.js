f43c29607684aea3cefacf5b27212a4cbec05876
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-19 17:43:36 +0530

    cart

--- models/product.js
+++ models/product.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const rootDir = require('../util/path')
 
-const p = path.join(rootDir, 'data' , 'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
@@ -23,7 +26,7 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString()
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
@@ -32,15 +35,14 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll(cb){
-    getProductsFromFile(cb)
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 
   static findById(id, cb) {
     getProductsFromFile(products => {
-      const product = products.find(p => p.id === id)
-      cb(product)
-    })
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
   }
-}
-
+};
